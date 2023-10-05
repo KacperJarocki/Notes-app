7d628a9bbd4f21c6cadc66048ff3eadccaa27ef7
Kacper Jarocki <kacper@MacBook-Air-Kacper.local>
2023-10-05 13:34:44 +0200

    cleaning

--- frontend/src/components/PrivateRoute/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-
-const PrivateRoute = (props) =>{
-    const tokken = localStorage.getItem("authToken");
-    return tokken ? props.children: <Navigate to ="/components/Login"/>;
-};
-export default PrivateRoute;
